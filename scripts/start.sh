a8b49c663f5067ede210c8256b4822ec0972f865
Przemyslaw Wit <dobrzeprzemyslane@gmail.com>
2020-06-10 13:24:47 +0200

    Add ingress-controller

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,12 +4,17 @@ set -e
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+function enable_ingres_on_minikube() (
+    minikube addons enable ingress
+)
+
 function app_start() (
     echo "Starting application..."
 
     cd ${SOURCE_DIR}/../application
 
     ./gradlew charts:namespace:appInstall
+    ./gradlew charts:ingress-controller:appInstall
     ./gradlew charts:frontend:appLoad
     ./gradlew charts:frontend:appInstall
 
