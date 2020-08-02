359a551be7e7bf192a2714db37584e067c2b332f
Przemyslaw Wit <dobrzeprzemyslane@gmail.com>
2020-08-02 11:42:17 +0200

    Visited regions page

--- application/components/frontend/src/index.jsx
+++ application/components/frontend/src/index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
 import 'react-rangeslider/lib/index.css';
+import 'react-svg-map/lib/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- application/components/frontend/src/initial-diagnose/components/steps/VisitedRegions.jsx
+++ application/components/frontend/src/initial-diagnose/components/steps/VisitedRegions.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { CheckboxSVGMap } from 'react-svg-map';
+import World from '@svg-maps/world';
 import GenericStep from 'initial-diagnose/components/GenericStep';
+
+import 'initial-diagnose/components/steps/styles/VisitedRegions.css';
+
+const RegionsDescription = styled.p.attrs({ className: 'regions-description' })`
+  text-align: center;
+  font-size: 15px;
+  margin-bottom: 40px;
+  font-weight: 100;
+  margin: 0;
+`;
 
 const VisitedRegions = ({ currentStep, totalSteps }) => (
   <GenericStep
@@ -8,7 +21,10 @@ const VisitedRegions = ({ currentStep, totalSteps }) => (
     currentStep={currentStep}
     totalSteps={totalSteps}
   >
-    VisitedRegions
+    <RegionsDescription>
+      Please select the region you live in and places you&apos;ve traveled to in the last 12 months.
+    </RegionsDescription>
+    <CheckboxSVGMap map={World} />
   </GenericStep>
 );
 
