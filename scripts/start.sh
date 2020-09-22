5799b1c7026dc295a2fd63867a9ad60405e51da8
Przemyslaw Wit <dobrzeprzemyslane@gmail.com>
2020-09-22 02:04:33 +0200

    Use custom Keycloak image in auth-service charts

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,19 +8,25 @@ function enable_ingres_on_minikube() (
     minikube addons enable ingress
 )
 
+function create_role_binding() {
+    kubectl create clusterrolebinding add-on-cluster-admin \
+        --clusterrole=cluster-admin --serviceaccount=kube-system:default
+}
+
 function app_start() (
     echo "Starting DocWIRE..."
 
     cd ${SOURCE_DIR}/../application
 
-    ./gradlew charts:namespace:appInstall
+    #./gradlew charts:namespace:appInstall
 
-    ./gradlew charts:auth-db:appInstall
+    #./gradlew charts:auth-db:appInstall
+    #./gradlew charts:auth-service:appLoad
     ./gradlew charts:auth-service:appInstall
 
-    ./gradlew charts:account-db:appInstall
-    ./gradlew charts:account-service:appLoad
-    ./gradlew charts:account-service:appInstall
+    #./gradlew charts:account-db:appInstall
+    #./gradlew charts:account-service:appLoad
+    #./gradlew charts:account-service:appInstall
 
     #./gradlew charts:frontend:appLoad
     #./gradlew charts:frontend:appInstall
@@ -30,4 +36,5 @@ function app_start() (
 )
 
 enable_ingres_on_minikube
+create_role_binding
 app_start
