a641a086175ebdcf3f38d759083af292b6eb6fd5
Przemyslaw Wit <dobrzeprzemyslane@gmail.com>
2020-10-03 15:05:17 +0200

    Uncomment all services in start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -22,33 +22,33 @@ function app_start() (
 
     # build_custom_images
 
-    #./gradlew charts:namespace:appInstall
+    ./gradlew charts:namespace:appInstall
 
-    #./gradlew charts:auth-db:appInstall
-    #./gradlew charts:auth-service:appInstall
+    ./gradlew charts:auth-db:appInstall
+    ./gradlew charts:auth-service:appInstall
 
-    #./gradlew charts:account-db:appInstall
-    #./gradlew charts:account-service:appLoad
-    #./gradlew charts:account-service:appInstall -PminikubeIp=${MINIKUBE_IP}
+    ./gradlew charts:account-db:appInstall
+    ./gradlew charts:account-service:appLoad
+    ./gradlew charts:account-service:appInstall -PminikubeIp=${MINIKUBE_IP}
 
-    #./gradlew charts:rabbit-mq:appInstall
+    ./gradlew charts:rabbit-mq:appInstall
 
-    #./gradlew charts:messages-db:appInstall
-    #./gradlew charts:messages-service:appLoad
-    #./gradlew charts:messages-service:appInstall -PminikubeIp=${MINIKUBE_IP}
+    ./gradlew charts:messages-db:appInstall
+    ./gradlew charts:messages-service:appLoad
+    ./gradlew charts:messages-service:appInstall -PminikubeIp=${MINIKUBE_IP}
 
-    #./gradlew charts:openvidu-redis:appInstall
-    #./gradlew charts:openvidu-coturn:appInstall
-    #./gradlew charts:openvidu-server:appInstall -PminikubeIp=${MINIKUBE_IP}
+    ./gradlew charts:openvidu-redis:appInstall
+    ./gradlew charts:openvidu-coturn:appInstall
+    ./gradlew charts:openvidu-server:appInstall -PminikubeIp=${MINIKUBE_IP}
 
-    #./gradlew charts:appointments-db:appInstall
-    #./gradlew charts:appointments-service:appLoad
+    ./gradlew charts:appointments-db:appInstall
+    ./gradlew charts:appointments-service:appLoad
     ./gradlew charts:appointments-service:appInstall -PminikubeIp=${MINIKUBE_IP}
 
     # minikube mount application/components/frontend:/frontend/src
 
-    #./gradlew charts:frontend:appLoad
-    #./gradlew charts:frontend:appInstall
+    ./gradlew charts:frontend:appLoad
+    ./gradlew charts:frontend:appInstall
 
     cd -
     echo "DocWIRE started."
