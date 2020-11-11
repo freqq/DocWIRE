86fb882ce453ad5ddf297abdb6eaffa063c1939c
Przemyslaw Wit <dobrzeprzemyslane@gmail.com>
2020-11-11 16:48:30 +0100

    Fix Stripe cli script

--- scripts/run_stripe_cli.sh
+++ scripts/run_stripe_cli.sh
@@ -5,7 +5,7 @@ set -e
 function run_stripe_cli() {
     MINIKUBE_IP=$(minikube ip)
     stripe listen \
-        --forward-to $MINIKUBE_IP/api/payment/webhook/checkout \
+        --forward-to https://$MINIKUBE_IP/api/payment/webhook/checkout \
         --skip-verify \
         --api-key sk_test_51HdE8mDV6EZPJrHHkftff05Uwa2RNhDQLmnzUbPHuTb4PfYJShQ2OoQHFyPyIWCe3mwL46DM7XECaO68BqDO4GaD001VY2kjNY \
         --load-from-webhooks-api
